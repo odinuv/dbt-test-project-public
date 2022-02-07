43dd5b52bbb211d3b4fddce599ab8d8de1947af5
adam.vyborny@keboola.com <adam.vyborny@keboola.com>
2022-02-07 12:58:43 +0000

    edited stg model

--- models/example/stg_model.sql
+++ models/example/stg_model.sql
@@ -1,27 +1,18 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+   with source as (
+        
+        select * from {{ source('WORKSPACE_xxxxx', 'in.c-ex-dummy.dummy') }}
+        
+    ),
+    
+    renamed as (
+        
+        select
+            id,
+            col1,
+            col2,
+            col3
+        from source
+    
+    )
+    
+    select * from renamed
